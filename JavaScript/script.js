29b23e94d07af17ce01eec0efb70b2ce00a269c1
Kenny Williams <kennywilliams@Kennys-MBP.cinci.rr.com>
2019-08-21 21:57:41 -0400

    breakpoints

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,7 +1,11 @@
-$('.carousel').carousel({
+$(document).ready(funtion() {
+  $('.carousel').carousel({
     dots: true,
     infinite: true,
     interval: 10,
+    speed: 500,
     slidesToShow: 3,
     slidesToScroll: 3,
+    cssEase: 'linear'
   });
+});
